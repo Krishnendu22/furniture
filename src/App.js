3a0304446809a222ef2b839b3fa563686c2664ce
Aditya Rawat <125037123+adityawillcode@users.noreply.github.com>
2024-03-13 23:31:52 +0545

    login signup added, auth-middleware added , routing added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Authentication from "./pages/Authentication";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" Component={<LandingPage/>} /> 
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars } from "react-icons/fa6";
+import { RxCross1 } from "react-icons/rx";
+function Header() {
+ const [isOpen, setIsOpen] = useState(false);
 
-const Header = () => {
-  return <div>Header</div>;
-};
+ return (
+    <nav className="bg-white shadow-lg">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-7">
+            <div>
+              <Link to={'/'} className="flex items-center py-5 px-2">
+                <span className="font-extrabold text-gray-900 text-xl">Furniture Shop</span>
+              </Link>
+            </div>
+            <div className="hidden md:flex items-center space-x-1">
+              <Link to={'/'} className="py-5 px-3 text-gray-700 text-sm">Home</Link>
+              <Link to={'/'} className="py-5 px-3 text-gray-700 text-sm">Shop</Link>
+              <Link to={'/'} className="py-5 px-3 text-gray-700 text-sm">About</Link>
+              <Link to={'/'} className="py-5 px-3 text-gray-700 text-sm">Contact</Link>
+              <Link to={'/auth/login'} className="py-5 px-3 text-gray-700 text-sm">Login</Link>
+
+            </div>
+          </div>
+          <div className="md:hidden flex items-center">
+            <button onClick={() => setIsOpen(!isOpen)} className="mobile-menu-button">
+              {isOpen ? <RxCross1 className="w-6 h-6" /> : <FaBars className="w-6 h-6" />}
+            </button>
+          </div>
+        </div>
+        {isOpen && (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              <Link to={'/'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700">Home</Link>
+              <Link to={'/'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700">Shop</Link>
+              <Link to={'/'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700">About</Link>
+              <Link to={'/'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700">Contact</Link>
+              <Link to={'/auth/login'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700">Contact</Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+ );
+}
 
 export default Header;
